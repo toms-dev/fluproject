9809269b9e79d2cd836779ec81780c8bd39f7de6
Sualty <lagouge@polytech.unice.fr>
2013-11-22 00:07:28 +0100

    j

--- compilation.sh
+++ compilation.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 javac src/simulation/*.java src/simulation/propagation/*.java src/simulation/propagation/illness/*.java src/simulation/propagation/behaviors/*.java src/simulation/beings/*.java src/simulation/beings/health/*.java src/app/*.java -d build
-
-java -cp build app.App
---
+++ exeution.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp build app.App

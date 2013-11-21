142b529edf57cf61dc8a9b682c697cfd43c8b6f9
Sualty <lagouge@polytech.unice.fr>
2013-11-21 23:37:54 +0100

    fichier à exécuter pour lancer l application

---
+++ lancement.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+javac src/simulation/*.java src/simulation/propagation/*.java src/simulation/propagation/illness/*.java src/simulation/propagation/behaviors/*.java src/simulation/beings/*.java src/simulation/beings/health/*.java src/app/*.java -d build
+
+java build.app.App
